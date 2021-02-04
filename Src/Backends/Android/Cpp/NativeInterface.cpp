334fc79290af3739348cb86dab179b3adefa815e
Omar El Sayyed <omar@nomone.com>
2021-02-04 23:08:54 +0200

    - Readability enhancement.

--- Src/Backends/Android/Cpp/NativeInterface.cpp
+++ Src/Backends/Android/Cpp/NativeInterface.cpp
@@ -1,11 +1,20 @@
 #include <jni.h>
 
+#define NATIVES_CLASS "com/nomone/wasm_machine/WasmMachineNatives"
+#define FORMATTED_NATIVES_CLASS Java_com_nomone_wasm_1machine_WasmMachineNatives_
+
+#define APPEND_AGAIN(a, b) a ## b
+#define APPEND(a, b) APPEND_AGAIN(a, b)
+
+#define JNI_CALL_PREFIX 								extern "C" JNIEXPORT
+#define JNI_FUNCTION_NAME_PREFIX(functionName) 			APPEND(FORMATTED_NATIVES_CLASS, functionName)
+#define JNI_FUNCTION_SIGNATURE_PREFIX					JNIEnv *env, jclass clazz
+
 extern "C" {
     #include <WasmMachine.h>
 };
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_nomone_wasm_1machine_WasmMachineNatives_nativeCompileWasmToBites(JNIEnv *env, jclass clazz) {
+JNI_CALL_PREFIX jstring JNICALL JNI_FUNCTION_NAME_PREFIX(nativeCompileWasmToBites)(JNI_FUNCTION_SIGNATURE_PREFIX) {
 
     char message[256];
     compileWasmToBites(message);
