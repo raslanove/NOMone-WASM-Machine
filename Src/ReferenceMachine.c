d90871871a5836fe46ccfad8004ce9fd4e96df57
Omar El Sayyed <omar@nomone.com>
2021-08-30 20:54:06 +0200

    - More work on parsing module.

--- Src/ReferenceMachine.c
+++ Src/ReferenceMachine.c
@@ -19,8 +19,7 @@ static boolean INLINE isWhiteSpace(char byte);
 static boolean INLINE isOneByteToken(char byte);
 static void INLINE skipWhiteSpaces(const char* string, int32_t* in_out_index);
 static boolean getToken(const char* logTag, const char* inputText, int32_t* in_out_index, const char* expectedToken, char* outputToken);
-static boolean parseModule(struct ParsingStructure* parsingStructure, int32_t watCodeByteIndex);
-static boolean parseModuleExpression(struct ParsingStructure* parsingStructure, int32_t watCodeByteIndex);
+static boolean parseModule(struct ParsingStructure* parsingStructure, int32_t* in_out_watCodeByteIndex);
 
 static void NWM_destroyReferenceMachine(struct NWM_WasmMachine *machine) {
     machine->alive = False;
@@ -33,7 +32,8 @@ static boolean parseWatCode(struct NWM_WasmMachine *machine, const char* watCode
     parsingStructure.machine = machine;
     parsingStructure.watCode = watCode;
 
-    boolean success = parseModule(&parsingStructure, 0);
+    int32_t watCodeIndex=0;
+    boolean success = parseModule(&parsingStructure, &watCodeIndex);
 
     return success;
 }
@@ -60,20 +60,16 @@ static boolean getToken(const char* logTag, const char* inputText, int32_t* in_o
 
     boolean shouldContinueParsing = True;
     if (!byte) {
-
         // Check for end-of-file,
         NCString.copy(outputToken, "EOF");
-        shouldContinueParsing = False;
 
     } else if (isOneByteToken(byte)) {
-
         // One byte tokens,
         outputToken[0] = byte;
         outputToken[1] = 0;
         (*in_out_index)++;
 
     } else {
-
         // Word tokens,
         int32_t outputIndex = 0;
         while (byte && !isWhiteSpace(byte) && !isOneByteToken(byte)) {
@@ -91,38 +87,72 @@ static boolean getToken(const char* logTag, const char* inputText, int32_t* in_o
     // Check if this is the expected token,
     // TODO: keep track of line number and current column to use in error reporting...
     if (expectedToken && !NCString.equals(outputToken, expectedToken)) {
-        NERROR(logTag, "Expected: \"%s\", found: \"%s\"", expectedToken, outputToken);
+        NERROR(logTag, "Expected: \"%s%s%s\", found: \"%s%s%s\"", NTCOLOR(HIGHLIGHT), expectedToken, NTCOLOR(STREAM_DEFAULT), NTCOLOR(HIGHLIGHT), outputToken, NTCOLOR(STREAM_DEFAULT));
         shouldContinueParsing = False;
     }
 
     // TODO: remove...
-    if (shouldContinueParsing) NLOGI(logTag, "found token: %s", outputToken);
+    if (shouldContinueParsing) NLOGI(logTag, "found token: %s%s", NTCOLOR(HIGHLIGHT), outputToken);
 
     return shouldContinueParsing;
 }
 
-static boolean parseModule(struct ParsingStructure* parsingStructure, int32_t watCodeByteIndex) {
+static boolean parseModule(struct ParsingStructure* parsingStructure, int32_t* in_out_watCodeByteIndex) {
 
-#define LOG_TAG "ReferenceMachine.parseModule()"
-#define ASSERT_TOKEN(expectedToken) if (!getToken(LOG_TAG, parsingStructure->watCode, &watCodeByteIndex, expectedToken, token)) return False
+    int32_t initialByteCodeIndex = *in_out_watCodeByteIndex;
+
+    #define LOG_TAG "ReferenceMachine.parseModule()"
+    #define ASSERT_TOKEN(expectedToken) \
+    if (!getToken(LOG_TAG, parsingStructure->watCode, in_out_watCodeByteIndex, expectedToken, token)) { \
+        *in_out_watCodeByteIndex = initialByteCodeIndex; \
+        return False; \
+    }
+    #define TEST_TOKEN(testValue) NCString.equals(token, testValue)
 
     // Parse,
     char token[MAX_TOKEN_LENGTH];
 
     // "(",
-    ASSERT_TOKEN("(");
+    ASSERT_TOKEN("(")
 
     // "module",
-    ASSERT_TOKEN("module");
+    ASSERT_TOKEN("module")
 
-    // ...xxx
+    // Parse child expressions,
+    // Note: this could be accelerated by using trees, but would compromise readability. Maybe
+    // use Flex and Bison for the accelerated (non-reference) machine.
+    int32_t childExpressionIndex = *in_out_watCodeByteIndex;
+    do {
+        // Get token,
+        ASSERT_TOKEN(0)
 
-    NLOGI(LOG_TAG, "yay!");
+        // ")",
+        if (TEST_TOKEN(")")) {
+            // TODO: finalize and save the module structure...
+            return True;
+        } else if (TEST_TOKEN("(")) {
+            // Check child expressions,
+            *in_out_watCodeByteIndex = childExpressionIndex;
+            // observe errors ...
+            // ....xxx
+            // return false if error occurred...
+        } else {
+            NERROR(LOG_TAG, "Expected: \"%s)%s\" or \"%s(%s\", found: \"%s%s%s\"", NTCOLOR(HIGHLIGHT), NTCOLOR(STREAM_DEFAULT), NTCOLOR(HIGHLIGHT), NTCOLOR(STREAM_DEFAULT), NTCOLOR(HIGHLIGHT), token, NTCOLOR(STREAM_DEFAULT));
+            return False;
+        }
+    } while (True);
+
     return True;
 }
 
-static boolean parseModuleExpression(struct ParsingStructure* parsingStructure, int32_t watCodeByteIndex) {
+static boolean parseType(struct ParsingStructure* parsingStructure, int32_t* in_out_watCodeByteIndex) {
 
+#undef LOG_TAG
+#define LOG_TAG "ReferenceMachine.parseType()"
+    int32_t initialWatCodeIndex = *in_out_watCodeByteIndex;
+    char token[MAX_TOKEN_LENGTH];
+
+    return True;
 }
 
 struct NWM_WasmMachine *NWM_createReferenceWasmMachine(struct NWM_WasmMachine *outputMachine) {
